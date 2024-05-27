16fa4dd5b89718c560a5654077a109ebf9d8c80c
Hazel Szoka <piotrszokacontact@gmail.com>
2024-05-27 15:59:38 +0200

    Change jest config so it supports js extensions

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,16 @@ export default {
     testEnvironment: 'node',
     testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
     moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+    moduleNameMapper: {
+        "^(\\.{1,2}/.*)\\.js$": "$1",
+    },
+    extensionsToTreatAsEsm: [".ts"],
+    transform: {
+        "^.+\\.(mt|t|cj|j)s$": [
+            "ts-jest",
+            {
+                "useESM": true
+            }
+        ]
+    },
 };
